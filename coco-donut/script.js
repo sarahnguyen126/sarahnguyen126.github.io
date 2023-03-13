1f259e95268039804b8eb0f8ea4226fca033b56a
sarahnguyen126 <sn24@pdx.edu>
2023-03-12 23:28:21 -0700

    update

--- coco-donut/script.js
+++ coco-donut/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".nav > li").click(function() {
+    $(".nav > li").click(function(console.log) {
         $(".nav > li").removeClass('current');
         $(this).addClass('current');
     });
