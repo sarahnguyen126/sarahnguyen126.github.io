13afa608536557d5039145078e48b475768b3691
sarahnguyen126 <sn24@pdx.edu>
2023-03-13 21:37:22 -0700

    added in a bit of mobile

--- coco-donut/script.js
+++ coco-donut/script.js
@@ -1,6 +1,26 @@
-$(document).ready(function() {
-    $(".nav > li").click(function(console.log) {
-        $(".nav > li").removeClass('current');
-        $(this).addClass('current');
-    });
+// $(document).ready(function () {
+//   $("nav li").click(function () {
+//     $("nav li").removeClass("current");
+//     $(this).addClass("current");
+//   });
+// });
+
+window.addEventListener('DOMContentLoaded', () => {
+
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			const id = entry.target.getAttribute('id');
+			if (entry.intersectionRatio > 0) {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('current');
+			} else {
+				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('current');
+			}
+		});
+	});
+
+	// Track all sections that have an `id` applied
+	document.querySelectorAll('section[id]').forEach((section) => {
+		observer.observe(section);
+	});
+	
 });
