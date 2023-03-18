8975837241cc3896893e8116a6044845509ebeb8
sarahnguyen126 <sn24@pdx.edu>
2023-03-18 10:53:10 -0700

    added in loading donut

--- coco-donut/script.js
+++ coco-donut/script.js
@@ -24,3 +24,11 @@ window.addEventListener('DOMContentLoaded', () => {
 	});
 	
 });
+
+window.addEventListener("load", () => {
+	const loader = document.querySelector(".loader");
+	loader.classList.add("loader-hidden");
+	loader.addEventListener("transitionend", () => {
+		document.body.removeChild("loader");
+	})
+})
