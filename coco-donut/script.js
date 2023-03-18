8e9d0755e511d19dc28920505b1bfcb9ade9e938
sarahnguyen126 <sn24@pdx.edu>
2023-03-18 16:49:43 -0700

    added in welcome line

--- coco-donut/script.js
+++ coco-donut/script.js
@@ -5,27 +5,27 @@
 //   });
 // });
 
-window.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      const id = entry.target.getAttribute("id");
-      if (entry.intersectionRatio > 0) {
-        document
-          .querySelector(`nav li a[href="#${id}"]`)
-          .parentElement.classList.add("current");
-      } else {
-        document
-          .querySelector(`nav li a[href="#${id}"]`)
-          .parentElement.classList.remove("current");
-      }
-    });
-  });
+// window.addEventListener("DOMContentLoaded", () => {
+//   const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//       const id = entry.target.getAttribute("id");
+//       if (entry.intersectionRatio > 0) {
+//         document
+//           .querySelector(`nav li a[href="#${id}"]`)
+//           .parentElement.classList.add("current");
+//       } else {
+//         document
+//           .querySelector(`nav li a[href="#${id}"]`)
+//           .parentElement.classList.remove("current");
+//       }
+//     });
+//   });
 
-  // Track all sections that have an `id` applied
-  document.querySelectorAll("section[id]").forEach((section) => {
-    observer.observe(section);
-  });
-});
+//   // Track all sections that have an `id` applied
+//   document.querySelectorAll("section[id]").forEach((section) => {
+//     observer.observe(section);
+//   });
+// });
 
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
@@ -36,3 +36,28 @@ window.addEventListener("load", () => {
     document.body.removeChild("loader");
   });
 });
+
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", navHighlighter);
+function navHighlighter() {
+  let scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop =
+      current.getBoundingClientRect().top + window.pageYOffset - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".navigation a[href*=" + sectionId + "]")
+        .classList.add("current");
+    } else {
+      document
+        .querySelector(".navigation a[href*=" + sectionId + "]")
+        .classList.remove("current");
+    }
+  });
+}
