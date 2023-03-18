7ac65483002327f626569683469183beb436d37b
sarahnguyen126 <sn24@pdx.edu>
2023-03-18 11:30:47 -0700

    fixed preloading screen

--- coco-donut/script.js
+++ coco-donut/script.js
@@ -5,30 +5,34 @@
 //   });
 // });
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      if (entry.intersectionRatio > 0) {
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.add("current");
+      } else {
+        document
+          .querySelector(`nav li a[href="#${id}"]`)
+          .parentElement.classList.remove("current");
+      }
+    });
+  });
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('current');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('current');
-			}
-		});
-	});
-
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
-	});
-	
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("section[id]").forEach((section) => {
+    observer.observe(section);
+  });
 });
 
 window.addEventListener("load", () => {
-	const loader = document.querySelector(".loader");
-	loader.classList.add("loader-hidden");
-	loader.addEventListener("transitionend", () => {
-		document.body.removeChild("loader");
-	})
-})
+  const loader = document.querySelector(".loader");
+  setTimeout(function () {
+    loader.classList.add("loader-hidden");
+  }, 2700);
+  loader.addEventListener("transitionend", () => {
+    document.body.removeChild("loader");
+  });
+});
